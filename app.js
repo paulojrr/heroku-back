af29bec9e2fbb155193e38abb8e260dbf5d2e4dd
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2021-02-03 01:34:29 -0300

    app

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://grades-back-project.herokuapp.com:8080',
   })
 );
 
